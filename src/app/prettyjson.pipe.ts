208c5486aae29f977af546ad8d5b7e0393a17f42
addepix <adriano.delpino@gmail.com>
2021-03-20 14:33:57 -0300

    correccion prettyjson

--- src/app/prettyjson.pipe.ts
+++ src/app/prettyjson.pipe.ts
@@ -8,7 +8,7 @@ export class PrettyjsonPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     let result = JSON.stringify(value, null, 2)
     if (result != undefined) {
-      result.replace(/ /g, '&nbsp;').replace(/\n/g, '<br/>');
+      result = result.replace(/ /g, '&nbsp;').replace(/\n/g, '<br/>');
     }
     return result
   }
