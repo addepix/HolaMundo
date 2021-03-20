17d330f2e4931ed83a9ac24f851c3f28e3b8b6cd
addepix <adriano.delpino@gmail.com>
2021-03-20 14:26:11 -0300

    Correccion del error en prettyjson

--- src/app/prettyjson.pipe.ts
+++ src/app/prettyjson.pipe.ts
@@ -6,9 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PrettyjsonPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    return JSON.stringify(value, null, 2)
-    .replace(/ /g, '&nbsp;')
-    .replace(/\n/g, '<br/>');
+    let result = JSON.stringify(value, null, 2)
+    if (result != undefined) {
+      result.replace(/ /g, '&nbsp;').replace(/\n/g, '<br/>');
+    }
+    return result
   }
 
 }
